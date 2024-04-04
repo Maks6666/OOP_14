4e450bffdb5e782b717809641a047ca6fc355868
Maks6666 <maxim.kucher2005@gmail.com>
2024-04-04 12:00:14 +0300

    1st task

--- main.py
+++ main.py
@@ -5,3 +5,35 @@
 # роботи з банківським рахунком, щоб гроші знялися або
 # зарахувалися тільки при виконанні певних умов
 # (наприклад, якщо гроші на рахунку є).
+
+class BankAccount:
+    def __init__(self, balance=0):
+        self._balance = balance
+
+    @property
+    def balance(self):
+        return self._balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self._balance += amount
+            print(f"{amount} deposited. New balance: {self._balance}")
+        else:
+            print("Incorrect amount for deposit.")
+
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self._balance:
+            self._balance -= amount
+            print(f"{amount} withdrawn. New balance: {self._balance}")
+        else:
+            print("Incorrect amount for withdrawal.")
+
+account = BankAccount(1000)
+print("Current balance:", account.balance)
+
+account.deposit(500)
+account.withdraw(200)
+account.withdraw(150)
+
+
+
